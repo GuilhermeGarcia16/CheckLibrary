7ec4aa39c8f69d4ade00188391f8e5b805c06bec
Guilherme Garcia <guilherme_garcia16@hotmail.com>
2025-04-15 21:14:48 -0300

    Feature - Select country in Create Author

---
+++ CheckLibrary/wwwroot/js/APIs.js
@@ -0,0 +1,13 @@
+﻿$.ajax({
+    type: 'GET',
+    url: "https://restcountries.com/v3.1/all?fields=name,flag",
+    dataType: 'json',
+    success: function (data) {
+        $.each(data, function (index, value) {
+            $('#nationality')
+                .append($("<option>")
+                    .attr("value", value.flag)
+                    .html(value.name.common + "-" + value.flag));
+        })
+    }
+})
