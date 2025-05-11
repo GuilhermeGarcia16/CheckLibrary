fb18b57a7a33c41c06bf561a9307bdd05daad047
Guilherme Garcia <guilherme_garcia16@hotmail.com>
2025-05-11 18:47:23 -0300

    feat/Exception-NotFoundException

---
+++ CheckLibrary/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace CheckLibrary.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
