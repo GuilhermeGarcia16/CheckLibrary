9545f032ccf3187bec913e073f6333046dace440
Guilherme Garcia <guilherme_garcia16@hotmail.com>
2025-06-08 15:13:40 -0300

    feat/API Transfer country API to BackEnd

--- CheckLibrary/Controllers/AuthorController.cs
+++ CheckLibrary/Controllers/AuthorController.cs
@@ -1,9 +1,10 @@
-﻿using CheckLibrary.Data;
+﻿using CheckLibrary.Data.API;
 using CheckLibrary.Models;
 using CheckLibrary.Services;
 using CheckLibrary.Services.Exceptions;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 using System.Data;
 using System.Diagnostics;
 
@@ -12,6 +13,10 @@ namespace CheckLibrary.Controllers
     public class AuthorController : Controller
     {
         private readonly AuthorService _authorService;
+
+        private const String baseUrl = "https://restcountries.com/v3.1/";
+
+        private const String param = "all?fields=name,flag";
 
         public AuthorController(AuthorService authorService)
         {
@@ -28,17 +33,23 @@ namespace CheckLibrary.Controllers
         // GET: AuthorController/Details/5
         public async Task<IActionResult> Details(int id)
         {
-            if (id == null) { return RedirectToAction(nameof(Error), new { message = "Id Not Provided" }); };
+            if (id == null) { return RedirectToAction(nameof(Error), new { message = "Id Not Provided" }); }
             Author author = await _authorService.FindByAsync(id);
 
-            if (author == null) { return RedirectToAction(nameof(Error), new { message = "Id Not Found" }); };
+            if (author == null) { return RedirectToAction(nameof(Error), new { message = "Id Not Found" }); }
 
             return View(author);
         }
 
         // GET: AuthorController/Create
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
+            List<Country> countries = await this.ComunicationAPICountry(baseUrl, param);
+
+            if (countries.Count != 0)
+            {
+                ViewBag.Options = this.PopulateNationality(countries);
+            }
             return View();
         }
 
@@ -47,10 +58,7 @@ namespace CheckLibrary.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Author author)
         {
-            if (!ModelState.IsValid)
-            {
-                return View();
-            }
+            if (!ModelState.IsValid) { return View();}
 
             await _authorService.InsertAsync(author);
             return RedirectToAction(nameof(Index));
@@ -59,10 +67,10 @@ namespace CheckLibrary.Controllers
         // GET: AuthorController/Edit/5
         public async Task<ActionResult> Edit(int? id)
         {
-            if (id == null) { return RedirectToAction(nameof(Error), new { message = "Id Not Provided" }); };
-            Author author = await _authorService.FindByAsync(id.Value);
+            if (id == null) { return RedirectToAction(nameof(Error), new { message = "Id Not Provided" }); }
 
-            if(author == null) { return RedirectToAction(nameof(Error), new { message = "Id Not Found" }); };
+            Author author = await _authorService.FindByAsync(id.Value);
+            if (author == null) { return RedirectToAction(nameof(Error), new { message = "Id Not Found" }); }
 
             return View(author);
         }
@@ -72,23 +80,20 @@ namespace CheckLibrary.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit(int id, Author author)
         {
-            if (!ModelState.IsValid)
-            {
-                return RedirectToAction(nameof(Index));
-            }
+            if (!ModelState.IsValid) { return RedirectToAction(nameof(Index)); }
 
-            if(id != author.Id) { return RedirectToAction(nameof(Index), new { message = "Id mismatch" }); };
+            if (id != author.Id) { return RedirectToAction(nameof(Index), new { message = "Id mismatch" }); }
 
             try
             {
                 await _authorService.UpdateAsync(author);
                 return RedirectToAction(nameof(Index));
             }
-            catch(NotFoundException ex)
+            catch (NotFoundException ex)
             {
                 return RedirectToAction(nameof(Error), new { message = ex.Message });
             }
-            catch(DBConcurrencyException exdb)
+            catch (DBConcurrencyException exdb)
             {
                 return RedirectToAction(nameof(Error), new { message = exdb.Message });
             }
@@ -119,6 +124,41 @@ namespace CheckLibrary.Controllers
         {
             var viewModel = new ErrorViewModel { Message = message, RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier };
             return View(viewModel);
+        }
+
+        protected virtual async Task<List<Country>> ComunicationAPICountry(String baseUrl, String param = "")
+        {
+            List<Country> countries = new List<Country>();
+
+            using (var cliente = new HttpClient())
+            {
+                cliente.BaseAddress = new Uri(baseUrl);
+                cliente.DefaultRequestHeaders.Clear();
+                HttpResponseMessage response = await cliente.GetAsync(param);
+                if (response.IsSuccessStatusCode)
+                {
+                    var Response = response.Content.ReadAsStringAsync().Result;
+                    countries = JsonConvert.DeserializeObject<List<Country>>(Response);
+                }
+            }
+
+            return countries;
+        }
+
+        private List<SelectListItem> PopulateNationality(List<Country> countries)
+        {
+            List<SelectListItem> nationality = new List<SelectListItem>();
+
+            foreach (var country in countries)
+            {
+                nationality.Add(new SelectListItem
+                    {
+                       Value = country.Flag.ToUpper(),
+                       Text = country.Name.Common.ToUpper()
+                    }
+                );
+            }
+            return nationality;
         }
     }
 }
---
+++ CheckLibrary/Data/API/Nationality.cs
@@ -0,0 +1,14 @@
+﻿namespace CheckLibrary.Data.API
+{
+   public record class NameCountry(
+       String? Common = null,
+       String? Official = null
+   );
+    /// <summary>Classe que recebe dados da API https://restcountries.com/v3.1/</summary>
+    /// <param name="Name"></param>
+    /// <param name="Flag"></param>
+    public record class Country(
+    NameCountry? Name = null,
+    String? Flag = null
+    );
+}
