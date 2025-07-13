0bbdd2c93d78f30167fe155f80162edcfa2cec20
Guilherme Garcia <guilherme_garcia16@hotmail.com>
2025-07-13 20:20:56 -0300

    feat/Interface - Interface to CRUD

---
+++ CheckLibrary/Services/IBaseRegister.cs
@@ -0,0 +1,11 @@
+﻿namespace CheckLibrary.Services
+{
+    public interface IBaseRegister<TModel>
+    {
+        public Task<List<TModel>> FindAllAsync();
+        public Task<TModel> FindByAsync(int id);
+        public Task InsertAsync(TModel author);
+        public Task UpdateAsync(TModel author);
+        public Task DeleteAsync(int id);
+    }
+}
