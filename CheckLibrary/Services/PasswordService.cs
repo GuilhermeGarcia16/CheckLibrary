bc8713576b18fca1031a0727c104dfb1cb694630
Guilherme Garcia <guilherme_garcia16@hotmail.com>
2025-08-26 20:09:58 -0300

    feat: Account/Login - Password Service

---
+++ CheckLibrary/Services/PasswordService.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CheckLibrary.Services
+{
+    public class PasswordService
+    {
+
+        public static string CriptographyPassword(string password)
+        {
+            var md5 = MD5.Create();
+            byte[] bytes = Encoding.ASCII.GetBytes(password);
+            byte[] hash = md5.ComputeHash(bytes);
+
+            var sb = new StringBuilder();
+
+            foreach (var caracter in hash)
+            {
+                sb.Append(caracter.ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+
+        public static Boolean VerifyPassword(string enterPassword, string userPassword)
+        {
+            string _enterPaswword = CriptographyPassword(enterPassword);
+            return _enterPaswword.Equals(userPassword);
+        }
+    }
+}
